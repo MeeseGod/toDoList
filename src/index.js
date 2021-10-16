32dd0206f9cb43f157bdee32e7f3421a878b36f7
JosephR <joseph_rundell@outlook.com>
2021-10-16 18:29:12 -0400

    Implemented module pattern

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {displayTasks, displayProjects} from "./modules/display"
+import display from "./modules/display"
 
 document.getElementById('addProject').addEventListener('click', function(){
     document.getElementById('projectCreationContainer').style.display = 'block';
@@ -11,8 +11,10 @@ document.getElementById('cancelProjectCreation').addEventListener('click', funct
 document.addEventListener("click", function(e){
     if(e.target.parentNode.className == 'projectDivs'){
         console.log(e.target.parentNode.id.replace("projectDiv", ""));
-        displayTasks();
+        display.displayTasks();
     }
 })
 
-displayProjects();
+display.testModule();
+
+display.displayProjects();
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,23 +1,26 @@
 import getProjects from "./projects";
 
-function displayProjects(){
-    let getProject = getProjects();
+const display = (() => {
+    const testModule = () => {console.log("Monkey")};
 
-    for(let i = 0; i < getProject.length; i++){
-        let createProjectDiv = document.createElement('div');
-        createProjectDiv.setAttribute('id', `projectDiv${i}`);
-        createProjectDiv.setAttribute('class', `projectDivs`);
-        let displayProjectName = document.createElement('p');
-        displayProjectName.textContent = `Project: ${getProject[i].projectName}`;
-        document.getElementById('projects').appendChild(createProjectDiv);
-        document.getElementById(`projectDiv${i}`).appendChild(displayProjectName);
-    }
-}
+    const displayProjects = () => {
+        const getProject = getProjects();
+        for(let i = 0; i < getProject.length; i++){
+            let createProjectDiv = document.createElement('div');
+            createProjectDiv.setAttribute('id', `projectDiv${i}`);
+            createProjectDiv.setAttribute('class', `projectDivs`);
+            let displayProjectName = document.createElement('p');
+            displayProjectName.textContent = `Project: ${getProject[i].projectName}`;
+            document.getElementById('projects').appendChild(createProjectDiv);
+            document.getElementById(`projectDiv${i}`).appendChild(displayProjectName);
+        }
+    };
 
-function displayTasks(index){
-    console.log("Displaying tasks");
-}
+    const displayTasks = () =>{
+        console.log("Displaying tasks");
+    };
 
-// export default displayProjects;
+    return {testModule, displayTasks, displayProjects};
+})();
 
-export {displayProjects, displayTasks}
+export default display
